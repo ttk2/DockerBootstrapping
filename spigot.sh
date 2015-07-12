d09004fec9ebd63a8b52c2bbdfa2b574d62c0763
ttk2 <ttk2@civcraft.co>
2015-07-12 18:12:04 -0500

    made launch script path absolute

--- spigot.sh
+++ spigot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:ParallelGCThreads=7 -Xmx5G -Xms2G -XX:UseSSE=3 -jar spigot.jar
+java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:ParallelGCThreads=7 -Xmx5G -Xms2G -XX:UseSSE=3 -jar /home/minecraft/spigot.jar
